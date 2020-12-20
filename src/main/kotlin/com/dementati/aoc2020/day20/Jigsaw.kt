a9102811400126b68cd8c282230c8f64d617ebe2
dementati <ztargaser@gmail.com>
2020-12-20 15:50:04 +0100

    Implement tile flipping and rotation

--- src/main/kotlin/com/dementati/aoc2020/day20/Jigsaw.kt
+++ src/main/kotlin/com/dementati/aoc2020/day20/Jigsaw.kt
@@ -18,8 +18,32 @@ class Tile constructor(val lines: List<String>) {
     var rotation = Rotation.D0
     var flipped = false
 
+    init {
+        assert(lines.isNotEmpty())
+        assert(lines.size == lines[0].length)
+    }
+
+    val dim = lines.size - 1
+
     fun get(x: Int, y: Int): Boolean {
-        return false
+        var xm: Double = x.toDouble()
+        var ym: Double = y.toDouble()
+        xm -= dim / 2.0
+        ym -= dim / 2.0
+
+        when (rotation) {
+            Rotation.D90 -> { xm = ym.also { ym = -xm } }
+            Rotation.D180 -> { xm = -xm.also { ym = -ym } }
+            Rotation.D270 -> { xm = -ym.also { ym = xm } }
+        }
+
+        if (flipped) {
+            xm = -xm
+        }
+
+        xm += dim / 2.0
+        ym += dim / 2.0
+        return lines[ym.toInt()][xm.toInt()] == '#'
     }
 
     fun matches(other: Tile, dir: Direction): Boolean {
--- src/test/kotlin/com/dementati/aoc2020/day20/TileTest.kt
+++ src/test/kotlin/com/dementati/aoc2020/day20/TileTest.kt
@@ -1,7 +1,9 @@
 package com.dementati.aoc2020.day20
 
+import com.dementati.aoc2020.Rotation
 import com.dementati.aoc2020.Tile
 import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class TileTest {
@@ -9,5 +11,131 @@ class TileTest {
     fun `get 1x1 empty`() {
         val tile = Tile(listOf("."))
         assertFalse(tile.get(0, 0))
+    }
+
+    @Test
+    fun `get 1x1 filled`() {
+        val tile = Tile(listOf("#"))
+        assertTrue(tile.get(0, 0))
+    }
+
+    @Test
+    fun `get 2x2`() {
+        val tile = Tile(listOf(
+            ".#",
+            ".#"
+        ))
+        assertFalse(tile.get(0, 0))
+        assertTrue(tile.get(1, 0))
+        assertFalse(tile.get(0, 1))
+        assertTrue(tile.get(1, 1))
+    }
+
+    @Test
+    fun `get 2x2 flipped`() {
+        val tile = Tile(listOf(
+            ".#",
+            ".#"
+        ))
+        tile.flipped = true
+        assertTrue(tile.get(0, 0))
+        assertFalse(tile.get(1, 0))
+        assertTrue(tile.get(0, 1))
+        assertFalse(tile.get(1, 1))
+    }
+
+    @Test
+    fun `get 2x2 rotated 90 degrees`() {
+        val tile = Tile(listOf(
+            "##",
+            ".."
+        ))
+        tile.rotation = Rotation.D90
+        assertFalse(tile.get(0, 0))
+        assertTrue(tile.get(1, 0))
+        assertFalse(tile.get(0, 1))
+        assertTrue(tile.get(1, 1))
+    }
+
+    @Test
+    fun `get 2x2 rotated 180 degrees`() {
+        val tile = Tile(listOf(
+            "##",
+            ".."
+        ))
+        tile.rotation = Rotation.D180
+        assertFalse(tile.get(0, 0))
+        assertFalse(tile.get(1, 0))
+        assertTrue(tile.get(0, 1))
+        assertTrue(tile.get(1, 1))
+    }
+
+    @Test
+    fun `get 2x2 rotated 270 degrees`() {
+        val tile = Tile(listOf(
+            "##",
+            ".."
+        ))
+        tile.rotation = Rotation.D270
+        assertTrue(tile.get(0, 0))
+        assertFalse(tile.get(1, 0))
+        assertTrue(tile.get(0, 1))
+        assertFalse(tile.get(1, 1))
+    }
+
+    @Test
+    fun `get 2x2 flipped and rotated 90 degrees`() {
+        val tile = Tile(listOf(
+            "#.",
+            ".."
+        ))
+        tile.flipped = true
+        tile.rotation = Rotation.D90
+        assertFalse(tile.get(0, 0))
+        assertFalse(tile.get(1, 0))
+        assertFalse(tile.get(0, 1))
+        assertTrue(tile.get(1, 1))
+    }
+
+    @Test
+    fun `get 2x2 flipped and rotated 180 degrees`() {
+        val tile = Tile(listOf(
+            "#.",
+            ".."
+        ))
+        tile.flipped = true
+        tile.rotation = Rotation.D180
+        assertFalse(tile.get(0, 0))
+        assertFalse(tile.get(1, 0))
+        assertTrue(tile.get(0, 1))
+        assertFalse(tile.get(1, 1))
+    }
+
+    @Test
+    fun `get 2x2 flipped and rotated 270 degrees`() {
+        val tile = Tile(listOf(
+            "#.",
+            ".."
+        ))
+        tile.flipped = true
+        tile.rotation = Rotation.D270
+        assertTrue(tile.get(0, 0))
+        assertFalse(tile.get(1, 0))
+        assertFalse(tile.get(0, 1))
+        assertFalse(tile.get(1, 1))
+    }
+
+    @Test
+    fun `get 3x3 flipped and rotated 90 degrees`() {
+        val tile = Tile(listOf(
+            "##.",
+            "...",
+            "..."
+        ))
+        tile.flipped = true
+        tile.rotation = Rotation.D90
+        assertFalse(tile.get(2, 0))
+        assertTrue(tile.get(2, 2))
+        assertTrue(tile.get(2, 1))
     }
 }
